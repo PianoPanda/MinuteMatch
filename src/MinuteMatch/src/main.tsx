8c0a68d498f5e3996bac5e193b1848786ccc3a60
Timmald-School <118050114+Timmald-School@users.noreply.github.com>
2025-03-31 15:46:49 -0400

    navbar 1

--- src/MinuteMatch/src/main.tsx
+++ src/MinuteMatch/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import Navbar from "./Navbar.tsx";
+import PostService from "./PostService.tsx"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+createRoot(root!).render(
+    <>
+    <BrowserRouter>
+    <Navbar />
+    <div>
+
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/post-service" element={<PostService/>}/>
+        </Routes>
+    </div>
+    </BrowserRouter>
+    </>
+);
