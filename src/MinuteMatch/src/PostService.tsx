2de6457c6a442e50f1f19e1ecb384cb0b4a8e7fa
Timmald-School <118050114+Timmald-School@users.noreply.github.com>
2025-04-02 15:02:25 -0400

    bare minimum post form

--- src/MinuteMatch/src/PostService.tsx
+++ src/MinuteMatch/src/PostService.tsx
@@ -1,6 +1,37 @@
 function PostService(){
+    const API_URL = "this_url_dont_work";
     return (
+        <>
         <h1>THIS IS WHERE YOU WOULD POST A SERVICE</h1>
+        <form>
+            <select name="type">
+                <option disabled selected="selected">Select a type</option>
+                <option>Test Type 1</option>
+                <option>Test Type 2</option>
+                <option>Test Type 3</option>
+            </select>
+            <br/>
+            <select name="category">
+                <option disabled selected="selected">Select a category</option>
+                <option>Test Cat 1</option>
+                <option>Test Cat 2</option>
+                <option>Test Cat 3</option>
+            </select>
+            <br/>
+            <select name="group">
+                <option disabled selected="selected">Select a group</option>
+                <option>Test Group 1</option>
+                <option>Test Group 2</option>
+                <option>Test Group 3</option>
+            </select>
+            <br/>
+            <textarea name="description" placeholder="Description"></textarea>
+            <br/>
+            <input type="file" name="picture"/>
+            <br/>
+            <button type="submit" formAction={API_URL}>SUBMIT</button>
+        </form>
+        </>
     )
 }
 
