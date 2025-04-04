573c98753ef95a88973d476ef855d8b6fdad6f43
Preston O'Connor <preston102003@gmail.com>
2025-04-04 10:34:40 -0400

    fixes for lab

--- src/MinuteMatch/src/components/blocks/ServiceCard.tsx
+++ src/MinuteMatch/src/components/blocks/ServiceCard.tsx
@@ -2,15 +2,14 @@ import {Service} from "../../types.ts";
 import "./ServiceCard.css";
 //TODO: Test picture
 function ServiceCard({service}:{service:Service}) {
-    // console.log(service);
-    // return(<div className="service-card">
-    //     <h3>{service.type}</h3>
-    //     {!service.picture?<p></p>:<img src={service.picture} alt="service-image"/>}
-    //     <i>Posted By <b>{service.user.name}</b></i>
-    //     <p><i>{service.group?service.group:""}</i></p>
-    //     <p>{service.category}</p>
-    //     <p>{service.description}</p>
-    // </div>)
+    return(<div className="service-card">
+        <h3>{service.type}</h3>
+        {!service.picture?<p></p>:<img src={service.picture} alt="service-image"/>}
+        <i>Posted By <b>{service.user.name}</b></i>
+        <p><i>{service.group?service.group:""}</i></p>
+        <p>{service.category}</p>
+        <p>{service.description}</p>
+    </div>)
 }
 
 export default ServiceCard;
