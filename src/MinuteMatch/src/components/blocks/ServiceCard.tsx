1f808831f4ddd94e5fde58e965f6666c5917632c
Preston O'Connor <preston102003@gmail.com>
2025-04-06 21:27:41 -0400

    Update Changes to get picture reading properties

--- src/MinuteMatch/src/components/blocks/ServiceCard.tsx
+++ src/MinuteMatch/src/components/blocks/ServiceCard.tsx
@@ -2,14 +2,15 @@ import {Service} from "../../types.ts";
 import "./ServiceCard.css";
 //TODO: Test picture
 function ServiceCard({service}:{service:Service}) {
-    return(<div className="service-card">
-        <h3>{service.type}</h3>
-        {!service.picture?<p></p>:<img src={service.picture} alt="service-image"/>}
-        <i>Posted By <b>{service.user.name}</b></i>
-        <p><i>{service.group?service.group:""}</i></p>
-        <p>{service.category}</p>
-        <p>{service.description}</p>
-    </div>)
+    // console.log(service);
+    // return(<div className="service-card">
+    //     <h3>{service.type}</h3>
+    //     {!service.picture?<p></p>:<img src={service.picture} alt="service-image"/>}
+    //     <i>Posted By <b>{service.user.name}</b></i>
+    //     <p><i>{service.group?service.group:""}</i></p>
+    //     <p>{service.category}</p>
+    //     <p>{service.description}</p>
+    // </div>)
 }
 
 export default ServiceCard;
