6f4aafbb60bdc48c0cc72809a185c3641128b98f
Timmald-School <118050114+Timmald-School@users.noreply.github.com>
2025-04-27 15:09:48 -0400

    Update create groups

--- src/MinuteMatch/src/supabase.js
+++ src/MinuteMatch/src/supabase.js
@@ -46,6 +46,59 @@ app.get('/group', async (req, res) => {
     res.json(group);
 });
 
+app.post('/group', async (req, res) => {
+//name members categories
+    console.log(req.body);
+    let {data:memberIDs,error1} = await supabase
+        .from("users")
+        .select('userid')
+        .likeAnyOf("username",req.body.members)
+    if(error1){
+        res.status(500).json(error)
+        return;
+    }
+    memberIDs = memberIDs.map(i=>i.userid)
+    let {data:catIDs,error2} = await supabase
+        .from("categories")
+        .select('id')
+        .likeAnyOf("name",req.body.categories);
+    if (error2){res.status(500).json(error)
+    return;}
+    catIDs=catIDs.map((i)=>i.id)
+
+    let { data: group, error } = await supabase
+        .from('group')
+        .insert({groupname:req.body.name})
+        .select();//list of objects with groupname attr
+    if (error){
+        console.error('Error adding group:', error);
+        res.status(500).json({ message: 'Error adding group' });
+        return;
+    }
+
+    await memberIDs.forEach(async (id)=>{
+        console.log(group)
+        let {error} = await supabase
+            .from("group_members")
+            .insert({group_id:group.groupid,member_id:id})
+        if(error){
+            console.error('Error adding group:', error);
+        }
+    });
+
+    await catIDs.forEach(async (id)=>{
+        console.log(id)
+        let {error} = await supabase
+            .from("group_categories")
+            .insert({group_id:group.groupid,category_id:id})
+        if(error){
+            console.error('Error adding group:', error);
+        }
+    });
+
+    res.json(group);
+});
+
 // **Fetch Categories**
 app.get('/categories', async (req, res) => {
         console.log("Do we even reach the back end to get to the fetch function!!!!!!");
@@ -136,19 +189,10 @@ app.post('/categories', async (req, res) => {
 // todo this will be implemented and used for the userlogin and also implemented and used for the ranking system**************************
 app.get('/user', async (req, res) => {
     const {data, error} = await supabase
-    .from('user')
+    .from('users')
     .select (
-        "UserID",
-        "Username",
-        "email", //todo do we even implement the email login for this set up???????
-        "ranking",
-        "verified",
-        "groups",
-        "last_active",
-        "flagged",
-        "reviews"
+        "userid,username,email,ranking,verified,groups,last_active,flagged,reviews"
     );
-
     if(error){
         console.error("Error fetching users:",error);
         return res.status(500).json({error: "Failed to fetch users"});
@@ -211,9 +255,7 @@ app.post('/user', async (req, res) => {
 // **Add a Post**
 app.post('/posts', upload.single('picture'), async (req, res) => {
         const { service, category, description, group} = req.body;
-        console.log(req.body);
         const picture = req.file ? req.file.buffer.toString("base64") : null; // <-- This is the fix: use the buffer instead of path
-        console.log(picture);
         if (!service) {
             return res.status(400).json({ error: 'Service type is required' });
         }
@@ -229,7 +271,6 @@ app.post('/posts', upload.single('picture'), async (req, res) => {
             .from("group")
             .select("groupid")
             .eq("groupname",group).limit(1).single():null;
-        console.log(groupID);
         let {data:result,error} = await supabase
         .from('posts')
         .insert({servicetype:service,text:description,picture:picture,groupid:groupID?groupID.data.groupid:null})
@@ -281,11 +322,9 @@ app.get('/posts', async (req, res) => {
             console.error('Error retrieving posts:', error);
             res.status(500).json({ error: 'Failed to fetch posts' });
         }
-        console.log(result);
         // Convert the bytea (binary data) to base64 so frontend can use it
         const posts = result.map(post => {
             let base64;
-            console.log(post.post_categories[0])
             if(post.picture) {
                 //console.log(/^[0-9a-fA-F]+$/.test(post.picture.slice(2)));
                 //console.log(post.picture.slice(0, 20));
