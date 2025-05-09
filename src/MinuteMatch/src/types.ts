c89f0c5a85b8bc437cc78cdccfa78d8823a3b082
Preston O'Connor <preston102003@gmail.com>
2025-05-09 03:10:53 -0400

    minor edit to the ranking page

--- src/MinuteMatch/src/types.ts
+++ src/MinuteMatch/src/types.ts
@@ -1,35 +1,4 @@
-// export interface Service {
-//     id: number;
-//     ServiceType: boolean;
-//     picture: string | null;
-//     user: {
-//         id: number;
-//         //name: string; //todo may need to update the 
-//     };
-//     groupId: number | null;
-//     category: string[];
-//     description: string;
-//     postComments: string[];
-//     timestamp: string;
-//     flagged:boolean;
-// }
 
-// export interface Service {
-//     id: number;
-//     ServiceType: boolean;
-//     picture: string | null;
-//     user: {
-//         id: number;
-//         // name: string; // still not used, but ok to keep if you need it later
-//     };
-//     groupId: number | null;
-//     category: string[];
-//     description: string;
-//     postComments: string[];
-//     timestamp: string;
-//     flagged: boolean;
-//     username?: string; // added this part so it is displayed in the card
-// }
 
 export interface Comment {
     username: string;
